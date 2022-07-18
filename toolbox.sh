d60f57bfa3ee86974227070ca42e42dabf3c86cf
didact <gmo25@cornell.edu>
2022-07-18 18:07:30 -0400

    reducing redundancies

---
+++ toolbox.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:stufus/egresscheck-framework.git
+git clone git@github.com:Tib3rius/AutoRecon.git
+git clone git@github.com:m3n0sd0n4ld/GooFuzz.git
+git clone git@github.com:aboul3la/Sublist3r.git
+git clone git@github.com:TryCatchHCF/PacketWhisper.git
